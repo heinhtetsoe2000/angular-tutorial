2627c7da2dfb54058466cf37daef131e3e4eb556
heinhtetsoe2000 <soe991398@gmail.com>
2022-07-24 22:05:25 +0630

    tour of heroes (angular official tutorial) - finished adding service.

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Homelander' },
+  { id: 2, name: 'Kimiko' },
+  { id: 3, name: 'Starlight' },
+  { id: 4, name: 'Doppelganger' },
+  { id: 5, name: 'Ezekiel' },
+  { id: 6, name: 'Stromfront' },
+  { id: 7, name: 'Mesmer' },
+  { id: 8, name: 'Popclaw' },
+  { id: 9, name: 'A-Train' },
+  { id: 10, name: 'The Deep' },
+  
+]
