4839bfc1b9b6dc7edcf70a52c3485fbdf922550e
heinhtetsoe2000 <soe991398@gmail.com>
2022-08-02 14:36:09 +0630

    reactive-form-app updated.

--- reactive-form-app/src/app/app.module.ts
+++ reactive-form-app/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -24,6 +29,11 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- reactive-form-app/src/app/components/test/test.component.ts
+++ reactive-form-app/src/app/components/test/test.component.ts
@@ -8,34 +8,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class TestComponent implements OnInit {
 
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]);
-  phone = new FormControl('', [Validators.required, Validators.pattern("[0-9]{11}")]);
-
-
-  getNameErrorMessage() {
-    if (this.name.hasError('required')) {
-      return 'Please enter your name';
-    } 
-
-    return this.name.hasError('name') ? 'Not a valid name' : '';
-  }
-
-  getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  getPhoneErrorMessage() {
-    if (this.phone.hasError('required')) {
-      return 'Please enter your phone number';
-    }
-
-    return this.phone.hasError('phone') ? 'Not a valide phone number' : '';
-  }
+  genders: string[] = ['Male', 'Female'];
 
   requiredForm: FormGroup = this.fb.group({});
 
@@ -46,6 +19,8 @@ export class TestComponent implements OnInit {
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
       phone: ['', [Validators.required, Validators.pattern("[0-9]{11}")]],
+      date: ['', Validators.required],
+      gender: ['', Validators.required]
     });
   }
 
