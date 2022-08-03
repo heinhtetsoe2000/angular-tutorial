160ffe08472eeb7d76dbbecfad1da8cc759500cd
heinhtetsoe2000 <soe991398@gmail.com>
2022-08-03 10:40:50 +0630

    template-driven-form finished

--- angular-service/src/app/app.component.ts
+++ angular-service/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from './services/product.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
   
 export class AppComponent {
